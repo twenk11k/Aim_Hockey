624d4b9274031425e65d0b1b748111f0f076b73d
twenk11k <selimdemiral@hotmail.com>
2019-02-05 11:34:29 +0300

    Make puck can move to left and right using keyboard

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,10 +8,27 @@ public class Player : MonoBehaviour
     public Transform boundaryHolder;
     Boundary playerBoundary;
     public Collider2D PlayerCollider { get; private set; }
+    [SerializeField] float yPush = 15f;
+    [SerializeField] float randomFactor = 0.2f;
 
+    public float maxSpeed;
 
     // nullable int
     public int? LockedFingerID { get; set; }
+
+    private float speed = -50f;
+
+    public GameObject arrow;
+    private bool hasStarted = false;
+
+
+    void FixedUpdate()
+    {
+         transform.Rotate(0, 0, Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,36 +48,63 @@ public class Player : MonoBehaviour
 
         rb.MovePosition(clampedMousePos);
     }
+    
     void Update()
     {
-        for (int i = 0; i < Input.touchCount; i++)
+        if (!hasStarted)
         {
-            Vector2 touchWorldPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
-                if (LockedFingerID == null)
-                {
-                    if (Input.GetTouch(i).phase == TouchPhase.Began
-                        && PlayerCollider.OverlapPoint(touchWorldPos))
-                    {
-                        LockedFingerID = Input.GetTouch(i).fingerId;
-                    }
-                }
-                else if (LockedFingerID == Input.GetTouch(i).fingerId)
-                {
-                    MoveToPosition(touchWorldPos);
-                    if (Input.GetTouch(i).phase == TouchPhase.Ended ||
-                        Input.GetTouch(i).phase == TouchPhase.Canceled)
-                    {
-                        LockedFingerID = null;
-                    }
-                }
-            
+            //LaunchOnMouseClick();
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-           // SceneManager.LoadScene("Menu");
-        }
-
+        // transform.Rotate(0, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
 
     }
 
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector2(0, yPush);
+            arrow.SetActive(false);
+            hasStarted = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2
+            (Random.Range(0f, randomFactor),
+            Random.Range(0f, randomFactor));
+        rb.velocity += velocityTweak;
+
+    }
+    private void TouchSlide()
+    {
+        /*
+     for (int i = 0; i < Input.touchCount; i++)
+     {
+         Vector2 touchWorldPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+             if (LockedFingerID == null)
+             {
+                 if (Input.GetTouch(i).phase == TouchPhase.Began
+                     && PlayerCollider.OverlapPoint(touchWorldPos))
+                 {
+                     LockedFingerID = Input.GetTouch(i).fingerId;
+                 }
+             }
+             else if (LockedFingerID == Input.GetTouch(i).fingerId)
+             {
+                 MoveToPosition(touchWorldPos);
+                 if (Input.GetTouch(i).phase == TouchPhase.Ended ||
+                     Input.GetTouch(i).phase == TouchPhase.Canceled)
+                 {
+                     LockedFingerID = null;
+                 }
+             }
+
+     }
+     if (Input.GetKeyDown(KeyCode.Escape))
+     {
+        // SceneManager.LoadScene("Menu");
+     }
+     */
+    }
 }
