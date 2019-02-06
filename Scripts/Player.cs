aa52f3b504b534a9c39de04f6b225a45d5f83cd0
twenk11k <selimdemiral@hotmail.com>
2019-02-06 23:25:35 +0300

    Add functionality to puck which can rotate via touching on mobile device screen

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
@@ -20,62 +19,88 @@ public class Player : MonoBehaviour
     public GameObject player;
 
     private bool hasStarted = false;
-    public float moveSpeed = 10f;
-    public float turnSpeed = -100f;
+    public float moveSpeed = 2.0f;
+    public float turnSpeed = -200f;
+    public float turnSpeedTouch = -1000f;
 
     int totalSizePucks;
 
-
-    // Start is called before the first frame update
+ 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         startingPosition = rb.position;
         PlayerCollider = GetComponent<Collider2D>();
-        totalSizePucks = Random.Range(5,8);
-        Debug.Log("transform position x is : "+ transform.position.x);
-        for(int i=0; i<totalSizePucks; i++)
-        {
-               Vector3 vector3 = new Vector3(transform.position.x + Random.Range(-2.21f, 2.21f), transform.position.y + Random.Range(-1.7f, -0.7f), transform.position.z);
-
-            GameObject player1 = Instantiate(player, vector3, transform.rotation);
-        }
+        //InstantiatePlayers();
     }
-
-
 
     void Update()
     {
-        LaunchOnMouseClick();
+        // LaunchOnMouseClick();
+        /*
+    if (Input.touches.Length > 0)
+    {
+        var t = Input.GetTouch(0);
+
+        if (t.phase == TouchPhase.Moved)
+        {
+               var delta = t.deltaPosition * turnSpeed * Time.deltaTime;
+
+               transform.Rotate(0,0,Mathf.Clamp(Mathf.Atan2(delta.y, delta.x), -1,1));
+
+            //transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg));
+
+        }
     }
+    */
+       
+
+    }
+
+    private void InstantiatePlayers()
+    {
+        totalSizePucks = Random.Range(4, 7);
+        Debug.Log("transform position x is : " + transform.position.x);
+        for (int i = 0; i < totalSizePucks; i++)
+        {
+            Vector3 vector3 = new Vector3(transform.position.x + Random.Range(-2.21f, 2.21f), transform.position.y + Random.Range(-1.7f, -0.7f), transform.position.z);
+
+            GameObject player1 = Instantiate(player, vector3, transform.rotation) as GameObject;
+        }
+    }
+
+   
     void FixedUpdate()
+    {
+         TouchControl();
+        
+    }
+
+    private void TouchControl()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             var touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            Debug.Log(Mathf.Clamp(touchDeltaPosition.x, -1, 1));
             //   transform.Translate(-touchDeltaPosition.x * Time.deltaTime * moveSpeed,
             //            -touchDeltaPosition.y * Time.deltaTime * moveSpeed, 0);
             //  var xpos = Mathf.Clamp(transform.position.x + ((touchDeltaPosition.x * Time.deltaTime * moveSpeed) / 10), xMin, xMax);
             //  var ypos = Mathf.Clamp(transform.position.y + ((touchDeltaPosition.y * Time.deltaTime * moveSpeed) / 10), yMin, yMax);
             //  transform.position = new Vector2(xpos, ypos);
-           // Debug.Log(Mathf.Clamp(transform.position.x + ((touchDeltaPosition.x * Time.deltaTime * moveSpeed),-1,1)));
-            transform.Rotate(0, 0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime);
+            Debug.Log(Mathf.Clamp(touchDeltaPosition.x, -1, 1));
+            transform.Rotate(0, 0, Mathf.Clamp(touchDeltaPosition.x, -1, 1) * turnSpeed * Time.deltaTime);
         }
-        else
-        {
-            
+       //   Debug.Log(Input.GetAxis("Horizontal"));
 
-        }
-        Debug.Log(Input.GetAxis("Horizontal"));
+       // transform.Rotate(0, 0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime);
 
-        transform.Rotate(0, 0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime);
+
+
 
         // rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
         //  transform.Rotate(Vector3.forward,0,Input.GetAxis("Horizontal")*speed*Time.deltaTime);
+
     }
-
-
-
 
     private void LaunchOnMouseClick()
     {
@@ -97,6 +122,25 @@ public class Player : MonoBehaviour
                 hasStarted = false;
             }
           
+        }
+        if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled))
+        {
+            if (!hasStarted)
+            {
+                float fRotation = rb.rotation * Mathf.Deg2Rad;
+                float fX = Mathf.Sin(fRotation);
+                float fY = Mathf.Cos(fRotation);
+                Vector2 v2 = new Vector2(fY * moveSpeed, fX * moveSpeed);
+                rb.velocity = v2;
+                arrow.SetActive(false);
+                hasStarted = true;
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, 0);
+                arrow.SetActive(true);
+                hasStarted = false;
+            }
         }
 
     }
@@ -140,4 +184,5 @@ public class Player : MonoBehaviour
      }
      */
     }
+    
 }
